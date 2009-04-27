8ea84693bc1feb7564cc7a40d0f4e96ef0ff9ba7
Kazuki Oikawa <k@oikw.org>
2009-04-27 13:32:43 +0000

    * p2pncsプロジェクトのスケルトンを追加

---
+++ p2pncs.simulation/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace p2pncs.simulation
+{
+	class Program
+	{
+		static void Main (string[] args)
+		{
+		}
+	}
+}
