6a409c720710f73b4a9706b61ce3eaaa1d49bc88
Kazuki Oikawa <k@oikw.org>
2009-04-29 11:09:05 +0000

    * IDatagramEventSocket.cs  * DatagramReceiveEventArgs.cs  * DatagramReceiveEventHandler.cs: データグラム専用ソケットのインターフェイスを追加
     * IMessagingSocket.cs
     * InquiredEventArgs.cs
     * InquiredEventHandler.cs: 問い合わせようのメッセージングソケットのインターフェイスを追加

---
+++ p2pncs.core/Net/IDatagramEventSocket.cs
@@ -0,0 +1,64 @@
+﻿/*
+ * Copyright (C) 2009 Kazuki Oikawa
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.Net;
+
+namespace p2pncs.Net
+{
+	/// <summary>
+	/// データグラム専用のソケット。
+	/// ただし、データグラムの受信は
+	/// <see cref="System.Net.Sockets.Socket"/>
+	/// と異なりReceiveFromメソッドを使うのではなく、
+	/// Receivedイベントをハンドリングすることによって行う。
+	/// </summary>
+	public interface IDatagramEventSocket : IDisposable
+	{
+		/// <summary>
+		/// 関連づけるローカルのエンドポイントを指定します。
+		/// </summary>
+		/// <param name="bindEP">関連づけるローカルエンドポイント</param>
+		void Bind (EndPoint bindEP);
+
+		/// <summary>
+		/// ソケットをクローズしすべてのリソースを解放します
+		/// </summary>
+		void Close ();
+
+		/// <summary>
+		/// データグラムを指定されたエンドポイントへ送信します
+		/// </summary>
+		/// <param name="buffer">送信するデータグラム</param>
+		/// <param name="remoteEP">宛先となるエンドポイント</param>
+		void SendTo (byte[] buffer, EndPoint remoteEP);
+
+		/// <summary>
+		/// データグラムを指定されたエンドポイントへ送信します
+		/// </summary>
+		/// <param name="buffer">送信するデータグラムが含まれる配列</param>
+		/// <param name="offset">送信するデータの開始位置</param>
+		/// <param name="size">送信するデータサイズ</param>
+		/// <param name="remoteEP">宛先となるエンドポイント</param>
+		void SendTo (byte[] buffer, int offset, int size, EndPoint remoteEP);
+
+		/// <summary>
+		/// データグラムの受信時に発生するイベント
+		/// </summary>
+		event DatagramReceiveEventHandler Received;
+	}
+}
---
+++ p2pncs.core/Net/IMessagingSocket.cs
@@ -0,0 +1,55 @@
+﻿/*
+ * Copyright (C) 2009 Kazuki Oikawa
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.Net;
+
+namespace p2pncs.Net
+{
+	/// <summary>
+	/// リモートエンドポイントに対して、リクエストとレスポンスで構成される問い合わせを行うソケット
+	/// </summary>
+	public interface IMessagingSocket
+	{
+		/// <summary>
+		/// 非同期問い合わせを開始します
+		/// </summary>
+		/// <param name="obj">リクエストとなるメッセージ</param>
+		/// <param name="remoteEP">問い合わせ先のエンドポイント</param>
+		/// <param name="callback">問い合わせ完了時に呼び出す<see cref="System.AsyncCallback"/>デリゲート</param>
+		/// <param name="state">コールバック時に渡される状態を表す<see cref="System.Object"/></param>
+		/// <returns>この非同期問い合わせの状態を表す<see cref="System.IAsyncResult"/></returns>
+		IAsyncResult BeginInquire (object obj, EndPoint remoteEP, AsyncCallback callback, object state);
+
+		/// <summary>
+		/// 非同期問い合わせを終了します
+		/// </summary>
+		/// <param name="ar">この非同期問い合わせの状態を表す<see cref="System.IAsyncResult"/></param>
+		/// <returns>問い合わせに対するレスポンス。問い合わせに失敗した場合は null が返る</returns>
+		object EndInquire (IAsyncResult ar);
+
+		event InquiredEventHandler Inquired;
+		void StartResponse (InquiredEventArgs args, object response, bool throwWhenAlreadySent);
+
+		event InquiredEventHandler InquiryFailure;
+		event InquiredEventHandler InquirySuccess;
+
+		IDatagramEventSocket BaseSocket { get; }
+
+		void Close ();
+	}
+}
