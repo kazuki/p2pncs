0185b0e303fb4b0fc2e56f96f71238199f3b6a77
Kazuki Oikawa <k@oikw.org>
2009-04-30 14:50:32 +0000

    * IDistributedHashTable.cs  * GetResult.cs  * ILocalHashTable.cs: DHT関係のインターフェイスを追加

---
+++ p2pncs.core/Net.Overlay.DHT/GetResult.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * Copyright (C) 2009 Kazuki Oikawa
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace p2pncs.Net.Overlay.DHT
+{
+	public class GetResult
+	{
+		Key _key;
+		object _value;
+		int _hops;
+
+		internal GetResult (Key key, object value, int hops)
+		{
+			_key = key;
+			_value = value;
+			_hops = hops;
+		}
+
+		public Key Key {
+			get { return _key; }
+		}
+
+		public object Value {
+			get { return _value; }
+		}
+
+		public int Hops {
+			get { return _hops; }
+		}
+	}
+}
