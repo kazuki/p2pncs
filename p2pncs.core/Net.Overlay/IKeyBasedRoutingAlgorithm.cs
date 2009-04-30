7cfe12eb48ba0084de3b6ab6e8152525e98d1065
Kazuki Oikawa <k@oikw.org>
2009-04-30 13:05:54 +0000

    * IKeyBasedRouter.cs  * IKeyBasedRoutingAlgorithm.cs  * NodeHandle.cs  * RoutingResult.cs: KBRのインターフェイスを追加

---
+++ p2pncs.core/Net.Overlay/IKeyBasedRoutingAlgorithm.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * Copyright (C) 2009 Kazuki Oikawa
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System.Collections.Generic;
+using System.Net;
+
+namespace p2pncs.Net.Overlay
+{
+	public interface IKeyBasedRoutingAlgorithm
+	{
+		void Join (IPEndPoint[] initialNodes);
+		void Close ();
+
+		Key ComputeDistance (Key x, Key y);
+		int ComputeRoutingLevel (Key x, Key y);
+		int MaxRoutingLevel { get; }
+
+		NodeHandle[] GetNextHopNodes (Key dest, int maxNum, Key exclude);
+		NodeHandle[] GetRandomNodes (int maxNum);
+		NodeHandle[] GetNeighbors (int maxNum);
+
+		void Touch (NodeHandle node);
+		void Fail (NodeHandle node);
+
+		void Setup (Key selfNodeId, IKeyBasedAlgorithmUpcall upcall);
+		void Stabilize ();
+
+		string DumpRoutingTable (HashSet<Key> aliveNodes);
+		void GetRoutingTableInfo (HashSet<Key> aliveNodes, out int aliveMainEntries, out int deadMainEntries, out int aliveSubEntries, out int deadSubEntries);
+	}
+}
---
+++ p2pncs.core/Net.Overlay/RoutingResult.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * Copyright (C) 2009 Kazuki Oikawa
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace p2pncs.Net.Overlay
+{
+	public class RoutingResult
+	{
+		NodeHandle[] _candidates;
+		int _hops;
+
+		public RoutingResult (NodeHandle[] candidates, int hops)
+		{
+			_candidates = candidates;
+			_hops = hops;
+		}
+
+		public NodeHandle[] RootCandidates {
+			get { return _candidates; }
+		}
+
+		public int Hops {
+			get { return _hops; }
+		}
+	}
+}
