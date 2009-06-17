8401d2d260efb54aa83cfa81cf037db48bc1ae19
Kazuki Oikawa <k@oikw.org>
2009-06-17 18:57:59 +0000

    MergeableFileHeaderのハッシュ値を考慮して、DHTのGetに応答したり、Put時に値をマージするように改良

--- p2pncs.core/Net.Overlay.DFS.MMLC/MMLC.cs
+++ p2pncs.core/Net.Overlay.DFS.MMLC/MMLC.cs
@@ -24,6 +24,7 @@ using p2pncs.Net.Overlay.Anonymous;
 using p2pncs.Net.Overlay.DHT;
 using p2pncs.Security.Cryptography;
 using p2pncs.Threading;
+using p2pncs.Utility;
 
 namespace p2pncs.Net.Overlay.DFS.MMLC
 {
@@ -62,7 +63,7 @@ namespace p2pncs.Net.Overlay.DFS.MMLC
 			_uploadSide.Accepting += new AcceptingEventHandler (AnonymousRouter_Accepting);
 			_uploadSide.Accepted += new AcceptedEventHandler (AnonymousRouter_Accepted);
 
-			dht.RegisterTypeID (typeof (Key), 2, new LocalHashTableValueMerger <Key> ());
+			dht.RegisterTypeID (typeof (DHTMergeableFileValue), 2, DHTMergeableFileValue.Merger);
 			_int.AddInterruption (RePut);
 
 			_ar.AddBoundaryNodeReceivedEventHandler (typeof (DHTLookupRequest), DHTLookupRequest_Handler);
@@ -73,19 +74,19 @@ namespace p2pncs.Net.Overlay.DFS.MMLC
 		void PublishMessage_Handler (object sender, BoundaryNodeReceivedEventArgs args)
 		{
 			PublishMessage msg = args.Request as PublishMessage;
-			for (int i = 0; i < msg.Keys.Length; i ++)
-				_dht.LocalPut (msg.Keys[i], TimeSpan.FromMinutes (5), args.RecipientKey);
+			for (int i = 0; i < msg.Values.Length; i ++)
+				_dht.LocalPut (msg.Values[i].Key, TimeSpan.FromMinutes (5), new DHTMergeableFileValue (args.RecipientKey, msg.Values[i].Hash));
 		}
 
 		void DHTLookupRequest_Handler (object sender, BoundaryNodeReceivedEventArgs args)
 		{
 			DHTLookupRequest req = args.Request as DHTLookupRequest;
-			_dht.BeginGet (req.Key, typeof (Key), delegate (IAsyncResult ar) {
+			_dht.BeginGet (req.Key, typeof (DHTMergeableFileValue), delegate (IAsyncResult ar) {
 				GetResult result = _dht.EndGet (ar);
-				List<Key> list = new List<Key> ();
+				List<DHTMergeableFileValue> list = new List<DHTMergeableFileValue> ();
 				if (result != null && result.Values != null) {
 					for (int i = 0; i < result.Values.Length; i ++) {
-						Key key = result.Values[i] as Key;
+						DHTMergeableFileValue key = result.Values[i] as DHTMergeableFileValue;
 						if (key != null)
 							list.Add (key);
 					}
@@ -186,10 +187,20 @@ namespace p2pncs.Net.Overlay.DFS.MMLC
 						return;
 				}
 
-				Key[] keys = new Key[Math.Min (10, _rePutList.Count)];
-				_rePutList.CopyTo (0, keys, 0, keys.Length);
-				_rePutList.RemoveRange (0, keys.Length);
-				_uploadSide.MessagingSocket.BeginInquire (new PublishMessage (keys), null, null, null);
+				int num = Math.Min (10, _rePutList.Count);
+				List<PublishData> values = new List<PublishData> ();
+				int count = 0;
+				using (_dbLock.EnterReadLock ()) {
+					for (int i = 0; i < _rePutList.Count && count < num; i++) {
+						KeyValuePair<MergeableFileHeader, List<MergeableFileRecord>> value;
+						if (!_db.TryGetValue (_rePutList[i], out value))
+							continue;
+						values.Add (new PublishData (_rePutList[i], value.Key.RecordsetHash));
+						count++;
+					}
+				}
+				_rePutList.RemoveRange (0, count);
+				_uploadSide.MessagingSocket.BeginInquire (new PublishMessage (values.ToArray ()), null, null, null);
 			}
 		}
 
@@ -323,20 +334,22 @@ namespace p2pncs.Net.Overlay.DFS.MMLC
 				Console.WriteLine ("I: DHTに問い合わせ中...");
 				IAsyncResult ar = _mmlc._uploadSide.MessagingSocket.BeginInquire (new DHTLookupRequest (_key != null ? _key : _kvPair.Key.Key), null, null, null);
 				DHTLookupResponse dhtres = _mmlc._uploadSide.MessagingSocket.EndInquire (ar) as DHTLookupResponse;
-				Console.WriteLine ("I: DHTから応答受信. 結果={0}件", dhtres == null || dhtres.Keys == null ? 0 : dhtres.Keys.Length);
-				if (dhtres == null || dhtres.Keys == null || dhtres.Keys.Length == 0)
+				Console.WriteLine ("I: DHTから応答受信. 結果={0}件", dhtres == null || dhtres.Values == null ? 0 : dhtres.Values.Length);
+				if (dhtres == null || dhtres.Values == null || dhtres.Values.Length == 0)
 					return;
-				foreach (Key value in dhtres.Keys)
-					Console.WriteLine ("I:   {0}", value.ToString ().Substring (0, 8));
+				foreach (DHTMergeableFileValue value in dhtres.Values)
+					Console.WriteLine ("I:   {0}/{1}", value.Holder.ToString ().Substring (0, 8), value.Hash.ToString ().Substring (0, 8));
 				bool connecting = false;
-				for (int i = 0; i < dhtres.Keys.Length; i++) {
+				for (int i = 0; i < dhtres.Values.Length; i++) {
 					try {
-						Console.WriteLine ("I: {0}へ接続", dhtres.Keys[i].ToString ().Substring (0, 8));
-						ar = _mmlc._ar.BeginConnect (_mmlc._uploadSide.Key, dhtres.Keys[i], AnonymousConnectionType.HighThroughput,
+						if (_key == null && _kvPair.Key.RecordsetHash.Equals (dhtres.Values[i].Hash))
+							continue;
+						Console.WriteLine ("I: {0}へ接続", dhtres.Values[i].Holder.ToString ().Substring (0, 8));
+						ar = _mmlc._ar.BeginConnect (_mmlc._uploadSide.Key, dhtres.Values[i].Holder, AnonymousConnectionType.HighThroughput,
 							_key != null ? (object)_key : (object)_kvPair.Key, null, null);
 						connecting = true;
 						break;
-					} catch { }
+					} catch {}
 				}
 				if (!connecting) {
 					Console.WriteLine ("I: 接続失敗'");
@@ -522,24 +535,42 @@ namespace p2pncs.Net.Overlay.DFS.MMLC
 
 		#region Messages
 		[SerializableTypeId (0x402)]
-		class MergeableEndPoint
+		class PublishData
 		{
-			// TODO
+			[SerializableFieldId (0)]
+			Key _key;
+
+			[SerializableFieldId (1)]
+			Key _hash;
+
+			public PublishData (Key key, Key hash)
+			{
+				_key = key;
+				_hash = hash;
+			}
+
+			public Key Key {
+				get { return _key; }
+			}
+
+			public Key Hash {
+				get { return _hash; }
+			}
 		}
 
 		[SerializableTypeId (0x403)]
 		class PublishMessage
 		{
 			[SerializableFieldId (0)]
-			Key[] _keys;
+			PublishData[] _values;
 
-			public PublishMessage (Key[] keys)
+			public PublishMessage (PublishData[] values)
 			{
-				_keys = keys;
+				_values = values;
 			}
 
-			public Key[] Keys {
-				get { return _keys; }
+			public PublishData[] Values {
+				get { return _values; }
 			}
 		}
 
@@ -563,15 +594,15 @@ namespace p2pncs.Net.Overlay.DFS.MMLC
 		class DHTLookupResponse
 		{
 			[SerializableFieldId (0)]
-			Key[] _keys;
+			DHTMergeableFileValue[] _values;
 
-			public DHTLookupResponse (Key[] keys)
+			public DHTLookupResponse (DHTMergeableFileValue[] values)
 			{
-				_keys = keys;
+				_values = values;
 			}
 
-			public Key[] Keys {
-				get { return _keys; }
+			public DHTMergeableFileValue[] Values {
+				get { return _values; }
 			}
 		}
 
@@ -604,6 +635,261 @@ namespace p2pncs.Net.Overlay.DFS.MMLC
 
 			public MergeableFileRecord[] Records {
 				get { return _records; }
+			}
+		}
+		#endregion
+
+		#region Internal Class
+		[SerializableTypeId (0x408)]
+		sealed class DHTMergeableFileValue : IEquatable<DHTMergeableFileValue>
+		{
+			[SerializableFieldId (0)]
+			Key _holder;
+
+			[SerializableFieldId (1)]
+			Key _hash;
+
+			public DHTMergeableFileValue (Key holder, Key hash)
+			{
+				_holder = holder;
+				_hash = hash;
+			}
+
+			public Key Holder {
+				get { return _holder; }
+			}
+
+			public Key Hash {
+				get { return _hash; }
+			}
+
+			public override int GetHashCode ()
+			{
+				return _holder.GetHashCode ();
+			}
+			
+			public bool Equals (DHTMergeableFileValue other)
+			{
+				return _holder.Equals (other._holder);
+			}
+
+			public static ILocalHashTableValueMerger Merger {
+				get { return ValueMerger.Instance; }
+			}
+
+			sealed class ValueMerger : ILocalHashTableValueMerger, IMassKeyDelivererValueGetter
+			{
+				public static ValueMerger Instance = new ValueMerger ();
+				ValueMerger () { }
+
+				#region ILocalHashTableValueMerger Members
+
+				public object Merge (object value, object new_value, DateTime expirationDate)
+				{
+					StoreValue store_value = value as StoreValue;
+					if (store_value == null)
+						store_value = new StoreValue ();
+					lock (store_value) {
+						store_value.Merge (new_value as DHTMergeableFileValue, expirationDate);
+					}
+					return store_value;
+				}
+
+				public object[] GetEntries (object value, int max_num)
+				{
+					lock (value) {
+						return (value as StoreValue).GetEntries (max_num);
+					}
+				}
+
+				public void ExpirationCheck (object value)
+				{
+					lock (value) {
+						(value as StoreValue).ExpirationCheck ();
+					}
+				}
+
+				#endregion
+
+				#region IMassKeyDelivererValueGetter Members
+
+				public DHTEntry[] GetSendEntries (Key key, int typeId, object value, int max_num)
+				{
+					lock (value) {
+						return (value as StoreValue).GetSendEntries (key, typeId, max_num);
+					}
+				}
+
+				public void UnmarkSendFlag (object value, object mark_value)
+				{
+					lock (value) {
+						(value as StoreValue).UnmarkSendFlag (mark_value as DHTMergeableFileValue);
+					}
+				}
+
+				#endregion
+
+				class StoreValue
+				{
+					Dictionary<Key, List<HolderInfo>> _dic = new Dictionary<Key, List<HolderInfo>> (); // Key is Hash
+					Dictionary<Key, HolderInfo> _dic2 = new Dictionary<Key,HolderInfo> (); // Key is Holder
+
+					public StoreValue ()
+					{
+					}
+
+					public void Merge (DHTMergeableFileValue new_value, DateTime expirationDate)
+					{
+						HolderInfo info;
+						List<HolderInfo> list;
+
+						if (_dic2.TryGetValue (new_value.Holder, out info)) {
+							if (info.ExpirationDate >= expirationDate)
+								return;
+
+							info.Extend (expirationDate);
+							if (info.Hash.Equals (new_value.Hash))
+								return;
+							
+							list = _dic[info.Hash];
+							for (int i = 0; i < list.Count; i ++) {
+								if (list[i].ExpirationDate <= DateTime.Now || info.Equals (list[i])) {
+									_dic2.Remove (list[i].Holder);
+									list.RemoveAt (i --);
+								}
+							}
+							if (list.Count == 0)
+								_dic.Remove (info.Hash);
+							info.Hash = new_value.Hash;
+						} else {
+							info = new HolderInfo (new_value, expirationDate);
+							_dic2.Add (info.Holder, info);
+						}
+
+						if (!_dic.TryGetValue (info.Hash, out list)) {
+							list = new List<HolderInfo> ();
+							_dic.Add (info.Hash, list);
+						}
+						list.Add (info);
+					}
+
+					public object[] GetEntries (int max_num)
+					{
+						List<object> result = new List<object> ();
+						int num = Math.Max (1, max_num / _dic.Count);
+						Random rnd = new Random ();
+						foreach (List<HolderInfo> list in _dic.Values) {
+							HolderInfo[] tmp = list.ToArray ().RandomSelection (num);
+							for (int i = 0; i < tmp.Length; i ++)
+								result.Add (tmp[i].Value);
+						}
+						return result.ToArray().RandomSelection (max_num);
+					}
+
+					public void ExpirationCheck ()
+					{
+						List<Key> removeHash = new List<Key> ();
+						foreach (KeyValuePair<Key,List<HolderInfo>> pair in _dic) {
+							List<HolderInfo> list = pair.Value;
+							for (int i = 0; i < list.Count; i++) {
+								if (list[i].ExpirationDate <= DateTime.Now) {
+									_dic2.Remove (list[i].Holder);
+									list.RemoveAt (i--);
+								}
+							}
+							if (list.Count == 0)
+								removeHash.Add (pair.Key);
+						}
+						for (int i = 0; i < removeHash.Count; i ++)
+							_dic.Remove (removeHash[i]);
+					}
+
+					public DHTEntry[] GetSendEntries (Key key, int typeId, int max_num)
+					{
+						List<HolderInfo> result = new List<HolderInfo> ();
+						int num = Math.Max (1, max_num / _dic.Count);
+						Random rnd = new Random ();
+						foreach (List<HolderInfo> list in _dic.Values) {
+							for (int i = 0, q = 0; i < list.Count && q < num; i ++) {
+								if (list[i].ExpirationDate <= DateTime.Now) {
+									_dic2.Remove (list[i].Holder);
+									list.RemoveAt (i --);
+									continue;
+								}
+								if (list[i].SendFlag) continue;
+								result.Add (list[i]);
+								list[i].SendFlag = true;
+								q ++;
+							}
+						}
+						HolderInfo[] array = result.ToArray ().RandomSelection (max_num);
+						DHTEntry[] entries = new DHTEntry [array.Length];
+						for (int i = 0; i < entries.Length; i ++)
+							entries[i] = new DHTEntry (key, typeId, array[i].Value, array[i].ExpirationDate);
+						return entries;
+					}
+
+					public void UnmarkSendFlag (DHTMergeableFileValue mark_value)
+					{
+						HolderInfo info;
+						if (_dic2.TryGetValue (mark_value.Holder, out info))
+							info.SendFlag = false;
+					}
+				}
+
+				class HolderInfo : IEquatable<HolderInfo>
+				{
+					DHTMergeableFileValue _value;
+					DateTime _expiration;
+					bool _sendFlag;
+
+					public HolderInfo (DHTMergeableFileValue value, DateTime expiration)
+					{
+						_value = value;
+						_expiration = expiration;
+						_sendFlag = false;
+					}
+
+					public Key Holder {
+						get { return _value.Holder; }
+					}
+
+					public Key Hash {
+						get { return _value.Hash; }
+						set { _value._hash = value;}
+					}
+
+					public DHTMergeableFileValue Value {
+						get { return _value; }
+					}
+
+					public DateTime ExpirationDate {
+						get { return _expiration; }
+					}
+
+					public bool SendFlag {
+						get { return _sendFlag; }
+						set { _sendFlag = value;}
+					}
+
+					public void Extend (DateTime new_expiration)
+					{
+						if (_expiration < new_expiration) {
+							_expiration = new_expiration;
+							_sendFlag = false;
+						}
+					}
+
+					public override int GetHashCode ()
+					{
+						return _value.GetHashCode ();
+					}
+
+					public bool Equals (HolderInfo other)
+					{
+						return _value.Equals (other._value);
+					}
+				}
 			}
 		}
 		#endregion
