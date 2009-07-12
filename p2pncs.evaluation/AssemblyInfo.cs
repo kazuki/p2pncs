7bcf56ec717b43d24f7692ec2bdb17a866fb262a
Kazuki Oikawa <k@oikw.org>
2009-07-12 13:31:51 +0000

    バージョン情報などの共通情報を別ファイルに分離し共通化

--- p2pncs.evaluation/AssemblyInfo.cs
+++ p2pncs.evaluation/AssemblyInfo.cs
@@ -1,33 +1,22 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿/*
+ * Copyright (C) 2009 Kazuki Oikawa
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
+using System.Reflection;
+
 [assembly: AssemblyTitle ("p2pncs.evaluation")]
 [assembly: AssemblyDescription ("p2pncs evalution program")]
-[assembly: AssemblyConfiguration ("")]
-[assembly: AssemblyCompany ("")]
 [assembly: AssemblyProduct ("p2pncs.evaluation")]
-[assembly: AssemblyCopyright ("Copyright © Kazuki Oikawa 2009")]
-[assembly: AssemblyTrademark ("")]
-[assembly: AssemblyCulture ("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible (false)]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion ("2.99.99.4")]
-[assembly: AssemblyFileVersion ("2.99.99.4")]
